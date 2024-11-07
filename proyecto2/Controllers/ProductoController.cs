5a0d2e9e29c380c3a36c0aaff83abdda255d86ca
Santi7870 <santiago.panchi@udla.edu.ec>
2024-11-06 22:58:46 -0500

    commit11

---
+++ proyecto2/Controllers/ProductoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tuProyecto.Controllers
+{
+    public class ProductoController : Controller
+    {
+        public IActionResult Details(string id)
+        {
+            // Aquí puedes obtener el producto de la base de datos
+            // por el id. Por ahora usamos un ejemplo.
+            ViewData["Title"] = "Hoodie Clásico";
+
+            return View();
+        }
+    }
+}
